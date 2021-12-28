fbb9912f52c45ea8093a3588e8a2c65a3e863f7b
jangajahnavi <67750560+jangajahnavi@users.noreply.github.com>
2021-12-28 18:11:12 +0530

    Delete dist directory

--- dist/esm/types/components/Srbox/Srbox.d.ts
+++
@@ -1,8 +0,0 @@
-import React from "react";
-declare const SearchBox: (props: {
-    placeholder: string | undefined;
-    handleChange: React.ChangeEventHandler<HTMLTextAreaElement | HTMLInputElement> | undefined;
-    handleOnClick: React.MouseEventHandler<HTMLDivElement> | undefined;
-    onClick: React.MouseEventHandler<SVGSVGElement> | undefined;
-}) => JSX.Element;
-export default SearchBox;
